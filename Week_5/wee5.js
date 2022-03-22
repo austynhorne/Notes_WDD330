377da58e93d303ecdc8438b9c6555400a8b2d355
austynhorne <austynhorne@byui.edu>
2022-03-21 22:43:51 -0600

    w

--- Week_5/wee5.js
+++ Week_5/wee5.js
@@ -2,7 +2,7 @@ fetch('https://austynhorne.github.io/Notes_WDD330/Week_5/questions.json')
 .then((response) => response.json())
 .then((jsObject) => {
   console.table(jsObject);
-  const teamname = jsObject["teamname"];
+  const teamname = jsObject["Teamname"];
   for (let i = 0; i < Teamname.length; i++) {
   }
   document.getElementById('name1').textContent = jsObject.Teamname[0].name;
